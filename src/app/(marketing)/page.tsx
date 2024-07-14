e1df147ad131ade2755536d7a460301fdfce9840
Hayato Yonehara <hayato.yonehara1229@gmail.com>
2024-07-14 09:03:04 +0900

    最初のページ作成

---
+++ src/app/(marketing)/page.tsx
@@ -0,0 +1,46 @@
+import { buttonVariants } from "@/components/ui/button"
+import { siteConfig } from "@/config/site"
+import { cn } from "@/lib/utils"
+import Link from "next/link"
+
+export default function IndexPage() {
+  return (
+    <>
+      <section className="mt-6 md:pt-10 lg:py-32 pb-8 mb:pb-12">
+        <div className="container text-center flex flex-col items-center gap-4 max-w-[64rem]">
+          <Link
+            href={siteConfig.links.x}
+            className="bg-muted px-4 py-1.5 rounded-2xl font-medium text-sm"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Xをフォローする
+          </Link>
+          <h1 className="font-extrabold text-3xl sm:text-5xl md:text-6xl lg:text-7xl">
+            Post Writer
+          </h1>
+          <p className="text-muted-foreground sm:text-xl leading-normal max-w-[42rem]">
+            このアプリケーションはNext.js
+            AppRouterで作られたものです。ユーザーは自由に投稿をポストすることができます。
+          </p>
+          <div className="space-x-4">
+            <Link
+              href={"/login"}
+              className={cn(buttonVariants({ size: "lg" }))}
+            >
+              始める
+            </Link>
+            <Link
+              href={siteConfig.links.github}
+              className={cn(buttonVariants({ size: "lg", variant: "outline" }))}
+              target="_blank"
+              rel="noreferrer" // _blank使うときはこの属性をつけておくとセキュリティ的に良い(使わないと遷移後のページで遷移元がバレたりする
+            >
+              GitHub
+            </Link>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
